888bf3209c88939fe9435bdcfa0624e0309e8fda
hamza saeed <hamzasaeed4689@gmail.com>
2022-04-06 01:18:19 +0500

    new commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mysql=require('mysql')
 
-const  db = mysql.createConnection({
+var db = mysql.createConnection({
     host:'us-cdbr-east-05.cleardb.net',
     user :'bc39792813b337',
     password:'5f185273',
@@ -14,13 +14,30 @@ const  db = mysql.createConnection({
 //     password:'',
 //     database:'uiit'
 // })
-db.connect((error)=>{
-    if(error){
-        console.log(error)
-    }else{
-        console.log('Mysql Connected.....')
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
     }
-})
+  });
+}
+
+handleDisconnect();
 
 
 module.exports=db
