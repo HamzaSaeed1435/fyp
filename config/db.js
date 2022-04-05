c804a89ea2296b82316498aef54c56f964b33e6d
hamza saeed <hamzasaeed4689@gmail.com>
2022-04-06 01:30:30 +0500

    new commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql=require('mysql')
 
-var db = {
+var db = mysql.createConnection({
     host:'us-cdbr-east-05.cleardb.net',
     user :'bc39792813b337',
     password:'5f185273',
     database:'heroku_e862a1fb6c8706e'
-};
+})
 
 // mysql://bc39792813b337:5f185273@us-cdbr-east-05.cleardb.net/heroku_e862a1fb6c8706e?reconnect=true
 // const  db = mysql.createConnection({
