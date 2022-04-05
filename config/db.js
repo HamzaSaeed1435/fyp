91bfe7e58bcc7a2b5592c365cc1e6af7da38dd77
hamza saeed <hamzasaeed4689@gmail.com>
2022-04-05 23:48:11 +0500

    new commit

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
 const mysql=require('mysql')
 
-// const  db = mysql.createConnection({
-//     host:'us-cdbr-east-05.cleardb.net',
-//     user :'bc39792813b337',
-//     password:'5f185273',
-//     database:'heroku_e862a1fb6c8706e'
-// })
+const  db = mysql.createConnection({
+    host:'us-cdbr-east-05.cleardb.net',
+    user :'bc39792813b337',
+    password:'5f185273',
+    database:'heroku_e862a1fb6c8706e'
+})
 
 // mysql://bc39792813b337:5f185273@us-cdbr-east-05.cleardb.net/heroku_e862a1fb6c8706e?reconnect=true
-const  db = mysql.createConnection({
-    host:'localhost',
-    user :'root',
-    password:'',
-    database:'uiit'
-})
+// const  db = mysql.createConnection({
+//     host:'localhost',
+//     user :'root',
+//     password:'',
+//     database:'uiit'
+// })
 db.connect((error)=>{
     if(error){
         console.log(error)
